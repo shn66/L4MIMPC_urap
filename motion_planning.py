1ae618a4a1097fb39b844ead4999742d225fc069
Chaarvi <goelchaarvi@gmail.com>
2023-10-17 17:19:28 -0700

    checking lower_arr size

--- motion_planning.py
+++ motion_planning.py
@@ -398,4 +398,4 @@ def run_simulations(num_iters, plot_steps):
         world.plot_problem(robot.state_traj, start, goal0)
 
 # FIXME: robot.state will clip into the edge of an obstacle, making u_sol invalid
-run_simulations(num_iters=1, plot_steps=False) # Make this true to see every plot
+run_simulations(num_iters=1, plot_steps=True) # Make this true to see every plot
