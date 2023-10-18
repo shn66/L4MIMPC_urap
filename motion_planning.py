834319014a96928d615cb9c4deeb671ea282880a
Josh Zhang <joshzhang@berkeley.edu>
2023-10-17 22:05:48 -0700

    Fixed array mismatch. TODO: fix u_sol error.

--- motion_planning.py
+++ motion_planning.py
@@ -179,7 +179,7 @@ class Robot:
                 self.local_obs.insert((new_lower, new_size), (lower_y[i], size_y[i]))
                 # add modified vals  ((      x_items      ), (       y_items       ))
 
-        print(f"\nDEBUG: detect_obs() done. local_obs:\n{[round(x, 4) for x in self.local_obs]}")
+        print(f"\nDEBUG: detect_obs() done. local_obs:\n{self.local_obs}")
     
 
     def update_state(self, acc_x, acc_y):
