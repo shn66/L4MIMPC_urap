6c7d97e6f857b8026e3dd395aaad271248022f9e
Josh Zhang <joshzhang@berkeley.edu>
2023-10-20 14:43:57 -0700

    updates, creating model_training.py

---
+++ model_training.py
@@ -0,0 +1,16 @@
+import torch
+import numpy as np
+import torch.nn as nn
+import torchvision as tv
+import matplotlib.pyplot as plt
+import torch.nn.functional as fn
+import torchvision.transforms as hrt
+import motion_planning
+
+def import_files():
+    ...
+
+def model_training():
+    ...
+
+model_training()
