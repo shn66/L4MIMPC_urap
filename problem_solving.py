08e4a83fc56c93de09106cc051f63ffe94a214cb
Siddharth Hari Nair <42875310+shn66@users.noreply.github.com>
2023-12-05 13:46:36 -0800

    added dagger

--- problem_solving.py
+++ problem_solving.py
@@ -4,6 +4,7 @@ import pickle
 import numpy as np
 import motion_planning as mp
 import training_models as tm
+from datetime import datetime
 
 MODEL = "BEST_norms=0_drops=0_weigh=0_activ=leaky_relu.pth"
 
@@ -88,11 +89,12 @@ def problem_solving(num_iters, plot_sol):
         world.solutions = []
 
 
-def relaxed_problem(dataset, use_model):
+def relaxed_problem(dataset, use_model, do_dagger=False):
+
     # Randomize start, get vars & params
     if use_model:
         # start = world.random_state(iters=100, bound=0.1)
-        start = [0.0, -0.5, 0.0, 0.0]
+        start = [0.0,random.uniform(-0.6,0.6), 0.0, 0.0]
     else:
         i = random.randint(0, dataset.size - 1) # Random sol @ index i
         start, obs_arr, bl_sol, bu_sol = dataset.sols[i]
@@ -103,6 +105,14 @@ def relaxed_problem(dataset, use_model):
     problem, vars, params = mp.motion_planning(world, robot, relaxed=True)
     state, input = vars
     bool_low, bool_upp, state0, goal0, lower_obs, upper_obs = params
+
+    full_problem, full_vars, full_params = mp.motion_planning(world, robot, relaxed=False)
+    f_state , f_input, f_bool_low , f_bool_upp  = full_vars
+    f_state0, f_goal0, f_lower_obs, f_upper_obs = full_params
+
+    B_problem, B_vars, B_params = mp.motion_planning(world, robot, relaxed=False, horizon=5)
+    B_state , B_input, _, _ = B_vars
+    B_state0, B_goal0, B_lower_obs, B_upper_obs = B_params
 
     dist = lambda x: np.linalg.norm(np.array(robot.state) - np.array(x))
 
@@ -149,13 +159,11 @@ def relaxed_problem(dataset, use_model):
 
         state_sol, input_sol = state.value, input.value
 
+        prev_dist = dist(goal)
+
         if not isinstance(state_sol, np.ndarray):
+            
             print("\nDEBUG: invalid solution. Solving backup problem")
-
-            B_problem, B_vars, B_params = mp.motion_planning(world, robot, relaxed=False, horizon=5)
-
-            B_state , B_input, _, _ = B_vars
-            B_state0, B_goal0, B_lower_obs, B_upper_obs = B_params
 
             B_state0.value = np.array(robot.state)
             B_goal0.value  = np.array(goal)
@@ -165,16 +173,56 @@ def relaxed_problem(dataset, use_model):
 
             B_problem.solve(verbose=False)
             state_sol, input_sol = B_state.value, B_input.value
-        
+            if do_dagger:
+                print("\nDEBUG: Doing data aggregation")
+                f_state0.value = np.array(robot.state)
+                f_goal0.value  = np.array(goal)
+                f_lower_obs.value = np.array(lower_cpy)
+                f_upper_obs.value = np.array(lower_cpy) + np.array(size_cpy)
+                full_problem.solve(verbose=False)
+                print(f"Status = {full_problem.status}")
+                print(f"Optimal cost = {round(full_problem.value, 2)}")
+                print(f"Solve time = {round(full_problem.solver_stats.solve_time, 4)}s")
+
+                
+                arnd = lambda x: np.around(x.value, 1).tolist() # Rounds values to X.0
+                bl_sol, bu_sol = [], []
+                
+                for i in range(world.MAX):
+                    bl_sol.append(arnd(f_bool_low[i]))
+                    bu_sol.append(arnd(f_bool_upp[i]))
+
+                world.solutions.append([robot.state, [lower_cpy, size_cpy], bl_sol, bu_sol])
+
+                if abs(dist(goal)-prev_dist)<1e-5:             # If the robot is stuck, use full solution
+                    state_sol, input_sol = f_state.value, f_input.value
+
+
         robot.update_state(input_sol[0][0], input_sol[1][0])
-        world.plot_problem(state_sol, start, goal)
+        if not do_dagger:
+            world.plot_problem(state_sol, start, goal)
+        
+    
+
+    
+    if do_dagger:
+        day_time=datetime.now().strftime("%m_%d-%I-%M-%S")
+        file = open(f"data/sol_dagger_{day_time}.pkl", "wb")
+        pickle.dump(world.solutions, file)
+        world.solutions = []
 
 
 if __name__ == "__main__":
     dataset = tm.Dataset()
     SOLVE = False
+    DAGGER = True
+    DAGGER_ITERS=100
     
     if SOLVE:
         problem_solving(num_iters=0, plot_sol=False)
     else:
-        relaxed_problem(dataset, use_model=True)
+        num_iters = DAGGER_ITERS if DAGGER else 1
+        for i in range(num_iters):
+            relaxed_problem(dataset, use_model=True, do_dagger=DAGGER)
+
+        
